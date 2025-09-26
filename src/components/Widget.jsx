443cf9bcbfe5dcc64b9f08f4bb56ec6ea76a15da
Elena <elena.kazakova124@gmail.com>
2025-09-26 18:16:31 +0300

    fix

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ChatBot from '@hexlet/chatbot-v2';
-import '@hexlet/chatbot-v2/styles';
 
 const Widget = ({ steps }) => {
   const safeSteps = Array.isArray(steps) ? steps : [];
-  
   return <ChatBot steps={safeSteps} />;
 };
 
