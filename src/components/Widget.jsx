7e1f2b1270e6ddccd8a2f37020d50ada7f92581a
Elena <elena.kazakova124@gmail.com>
2025-09-29 16:58:23 +0300

    fix styles

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ChatBot from '@hexlet/chatbot-v2';
-import '@hexlet/chatbot-v2/styles';
+//import '@hexlet/chatbot-v2/styles';
 
 
 const Widget = ({ steps }) => {
