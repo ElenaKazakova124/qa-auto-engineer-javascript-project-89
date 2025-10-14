9f65a85b26c908c9e6954b851d33917c054ef3ec
Elena <elena.kazakova124@gmail.com>
2025-10-14 09:31:52 +0300

    fix import3

---
+++ jest-setup.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+
+global.console = {
+  ...console,
+  error: jest.fn(),
+  warn: jest.fn()
+};
+
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+  observe() {
+    return null;
+  }
+  disconnect() {
+    return null;
+  }
+  unobserve() {
+    return null;
+  }
+};
+
+global.matchMedia = global.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
+  };
+};
