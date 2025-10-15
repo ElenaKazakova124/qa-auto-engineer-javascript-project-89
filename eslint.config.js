818c11a0460e49378b4018082153b69ebf7c5edd
Elena <elena.kazakova124@gmail.com>
2025-10-15 19:46:13 +0300

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -8,11 +8,12 @@ import { fileURLToPath } from 'url'
 import vitest from '@vitest/eslint-plugin'
 
 const gitIgnorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
-// const eslintIgnorePath = fileURLToPath(new URL('.eslintignore', import.meta.url))
 
 export default defineConfig([
   includeIgnoreFile(gitIgnorePath),
-  // includeIgnoreFile(eslintIgnorePath),
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**', 'public/**', '*.config.js', '*.config.mjs', '*.setup.js'],
+  },
   stylistic.configs.recommended,
   { files: ['**/*.{js,mjs,cjs,jsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,jsx}'], languageOptions: { globals: globals.browser } },
